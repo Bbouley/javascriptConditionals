38f53a723bc677f70c022a3845ecc72613550ff8
bbouley <b.bouley@hotmail.co.uk>
2015-07-10 15:08:47 -0600

    added postal code verifier

--- conditional.js
+++ conditional.js
@@ -1,4 +1,4 @@
-var num = prompt('what is your number with dashes');
+var num = prompt('what is your number with dashes?');
 
 if (num.charAt(3) && num.charAt(7) === '-'){
   alert('This is a valid Phone Number');
@@ -6,10 +6,18 @@ if (num.charAt(3) && num.charAt(7) === '-'){
     alert('This is not a valid Phone Number');
 };
 
-var birthdate = prompt('what is your DOB, in the format 00/00/00');
+var birthdate = prompt('What is your DOB, in the format 00/00/00?');
 
-if (birthdate.charAt(2) && birthdate.charAt(5) === '/'){
+if (birthdate.charAt(2) && birthdate.charAt(5) === '/' && birthdate.length === 8){
   alert('Valid Birth Date');
 } else {
-    alert('Not a Valid Birth Date');
+    alert('Invalid Birth Date');
   }
+
+var postalcode = prompt('What is your postal code?');
+
+if (postalcode.charAt(5) === '-' || postalcode.length === 5){
+  alert('Valid Postal Code');
+} else {
+  alert('Invalid Postal Code');
+}
