b78dd66f19729d81fa7839f9896ba59cedc4364c
bbouley <b.bouley@hotmail.co.uk>
2015-07-10 14:56:25 -0600

    created blank html file and added phone number verifier

---
+++ conditional.js
@@ -0,0 +1,7 @@
+var num = prompt('what is your number with dashes');
+
+if (num.charAt(3) === '-' && num.charAt(7) === '-'){
+  alert('This is a valid Phone Number')}
+  else{
+    alert('This is not a valid Phone Number');
+};
